b8b1f1e6369da46b2cfe26fededc6a85a20ec7a4
bethannezink <bethannezink@gmail.com>
2017-08-03 20:36:54 -0400

    update data retrieval by page count

--- app/services/press_release_service.rb
+++ app/services/press_release_service.rb
@@ -1,11 +1,15 @@
 class PressReleaseService
 
-  def retrieve_data
-    HTTParty.get('http://www.justice.gov/api/v1/press_releases.json?pagesize=2')
+  def page_count
+    retrieve_data["metadata"]["resultset"]["count"]
+  end
+
+  def retrieve_data(url_params = '')
+    HTTParty.get('http://www.justice.gov/api/v1/press_releases.json?pagesize=100&sort=date' + url_params)
   end
 
   def results
-    retrieve_data["results"]
+    retrieve_data('&page=' + page_count)["results"]
   end
 
   def build_results
